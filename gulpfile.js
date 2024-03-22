51bea625c04cfdfc737dd454bcf7007895c694d7
Alisa Fedorova <2012alis@mail.ru>
2024-03-22 18:12:35 +0300

    Добавлен browserSync и слежка за файлами watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,28 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
 
+const browserSync = require('browser-sync').create();
+
+const watch = require('gulp-watch');
+
+//обновление указанного афйла (слежка за ним)
+
+gulp.task('watch', function () {
+    watch(['./app/*.html', './app/css/*.css'], gulp.parallel(browserSync.reload));
+});
+
+//задача для старта сервера из папки
+
+gulp.task('server', function () {
+    browserSync.init({
+        server: {
+            baseDir: './app/'
+        }
+    })
+});
+
+gulp.task('default', gulp.parallel('server', 'watch'));
+
+/*
 gulp.task('first', function (callback) {
     console.log('Hello from THIRD task');
     callback();
@@ -20,4 +43,4 @@ gulp.task('fourth', function (callback) {
     callback();
 })
 
-gulp.task('default', gulp.series('first', 'second', 'third', 'fourth'));
+gulp.task('default', gulp.series('first', 'second', 'third', 'fourth')); */
