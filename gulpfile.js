eb6bd7437b08f50e49b754bc1df806171357c5c1
Alisa Fedorova <2012alis@mail.ru>
2024-03-29 11:28:42 +0300

    Добавлена обработка ошибок scss

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,39 @@ const browserSync = require('browser-sync').create();
 const watch = require('gulp-watch');
 
 const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+
+const plumber = require('gulp-plumber');
+const notify = require("gulp-notify");
 
 // Таск для компиляции SCSS в CSS
 gulp.task('scss', function (callback) {
+
     return gulp.src('./app/scss/main.scss')
+        .pipe(plumber({
+            errorHandler: notify.onError(function (err) {
+                return {
+                    title: 'Styles',
+                    sound: false,
+                    message: err.message
+                }
+            })
+
+
+        }))
+        .pipe(sourcemaps.init())
         .pipe(sass())
+        .pipe(autoprefixer({
+            overrideBrowsersList: ['last 4 versions']
+        }))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('./app/css/'))
     callback();
 });
+
+
+
 // const sass = require('gulp-sass');
 
 // gulp.task('scss', function (callback) {
@@ -24,6 +49,13 @@ gulp.task('scss', function (callback) {
 
 gulp.task('watch', function () {
     watch(['./app/*.html', './app/css/**/*.css'], gulp.parallel(browserSync.reload));
+    // watch(' ./app/scss/**/*.scss', gulp.parallel('scss'));
+
+    //способ для обновления scss, чтобы все изменения применялись при сохранении изменений
+    watch('./app/scss/**/*.scss', function () {
+        setTimeout(gulp.parallel('scss'), 1000)
+    })
+
 });
 
 //задача для старта сервера из папки
@@ -36,7 +68,7 @@ gulp.task('server', function () {
     })
 });
 
-gulp.task('default', gulp.parallel('server', 'watch'));
+gulp.task('default', gulp.parallel('server', 'watch', 'scss'));
 
 /*
 gulp.task('first', function (callback) {
